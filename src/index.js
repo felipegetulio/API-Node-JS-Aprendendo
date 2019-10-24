ecb5c165512fcc04b995566283bd7fce3a49de18
Felipe Nascimento <felipegetulio15@gmail.com>
2019-10-24 16:29:15 -0400

    Registro de Usuario

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+require('./controllers/authController')(app);
+
+//exemplo
+//app.get('/', (req, res) => {
+	//req: dados da requisicao (parametros, token)
+	//res: objeto para utilizar pra enviar alguma resposta pro usuario
+//	res.send('OK');
+
+//});
+
+app.listen(3000);
